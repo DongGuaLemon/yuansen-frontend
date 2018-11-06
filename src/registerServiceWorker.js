33d80dffdcc72c295cd293a6af417073e4a2778c
ZongWei <g0970216929@gmail.com>
2018-11-06 23:08:03 +0800

    registerserviceworker

---
+++ src/registerServiceWorker.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-console */
+
+import { register } from 'register-service-worker'
+console.log("123");
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+//       console.log('SW registered: ', registration);
+//     }).catch(registrationError => {
+//       console.log('SW registration failed: ', registrationError);
+//     });
+//   });
+// }
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}service-worker.js`,{
+    ready () {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    cached () {
+      console.log('Content has been cached for offline use.')
+    },
+    updated () {
+      console.log('New content is available; please refresh.')
+    },
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
+}
+// register('/registerServiceWorker.js', {
+//   ready (registration) {
+//     console.log('Service worker is active.')
+//   },
+//   registered (registration) {
+//     console.log('Service worker has been registered.')
+//   },
+//   cached (registration) {
+//     console.log('Content has been cached for offline use.')
+//   },
+//   updatefound (registration) {
+//     console.log('New content is downloading.')
+//   },
+//   updated (registration) {
+//     console.log('New content is available; please refresh.')
+//   },
+//   offline () {
+//     console.log('No internet connection found. App is running in offline mode.')
+//   },
+//   error (error) {
+//     console.error('Error during service worker registration:', error)
+//   }
+// })
