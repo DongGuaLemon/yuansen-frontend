b18da52b8a8761b5897267e27c81714ef57f9246
ZongWei <g0970216929@gmail.com>
2018-11-06 23:10:31 +0800

    build

---
+++ dist/precache-manifest.d532684c10f29b442d0883859034798c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9abd1dfaf486fe076fd2",
+    "url": "/static/js/index.e801cb63.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9abd1dfaf486fe076fd2",
+    "url": "/static/css/index.0a14a6b6.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "503e213b773fbf8b148b9c0ddc7c0ce4",
+    "url": "/index.html"
+  }
+];
