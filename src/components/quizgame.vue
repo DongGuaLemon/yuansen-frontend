7c3a53355090b20099a53cb701cd0a3d1cb8fd0c
ZongWei <g0970216929@gmail.com>
2018-11-21 00:47:02 +0800

    rwd & css animation

--- src/components/quizgame.vue
+++ src/components/quizgame.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="quizgame">
+    <answeranimation ref="reference"></answeranimation>
     <b-container>
         <b-row class="justify-content-md-center mb-5">
           <b-col cols="2" md="1" lg="0"></b-col>
-          <b-col cols="8"  md="6" lg="6"><b-progress class="progress w-60 mb-5" :value="counter" :max="max" show-value animated></b-progress></b-col>
+          <b-col class="pro" cols="8"  md="6" lg="8"> <b-progress :max="max" show-value animated><b-progress-bar class="progress w-40 mb-5" :label="'第'+counter+'題'" :value="counter"></b-progress-bar></b-progress></b-col>
           <b-col offset="3" offset-md="1" offset-lg="0" class="timer"><div class="circle"><span class="timer1">{{timer}}</span></div></b-col>
         </b-row>
       <b-container>
-        <b-row class="mb-2">
-          <b-col>
+        <b-row class="mb-1">
+          <b-col class="cardcol">
           <b-card>
             <b-media no-body>
               <b-media-aside vertical-align="center">
@@ -29,12 +30,14 @@
             <b-col 
                     v-for="(item,index) in button" 
                     :key="item.id"
+                    :class="button_(index)"
                     > 
               <button
-                    @click="sub(index)" 
+                    @click="sub(index,item.id,item.isactive)" 
                     :disabled="item.correct" 
-                    class="btn button1" 
-                    :class="{correct:item.isactive}"
+                    :uid="item.id"
+                    class="btn" 
+                    
                     >{{item.ans}}
               </button>
             </b-col>
@@ -47,25 +50,29 @@
   </div>
 </template>
 <script>
+/*let removeadd = function(e){
+  this.classList.remove('playing')
+}*/
+import answeranimation from './answeranimation.vue'
 export default {
   data() {
     return {
-      counter: 8,
+      counter: 7,
       max:10,
       timer:10,
       topic:{
-        main:"Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque antesollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felisin faucibus.",
+        main:"Cras . Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felisin faucibus.",
         imgsrc:"",
       },
       button:[
-        {ans:"answer1",isactive:true},
-        {ans:"answer2",isactive:false},
-        {ans:"answer3",isactive:false},
-        {ans:"answer4",isactive:false}
+        {ans:"answer1",isactive:true,id:"1"},
+        {ans:"answer2",isactive:false,id:"2"},
+        {ans:"answer3",isactive:false,id:"3"},
+        {ans:"answer4",isactive:false,id:"4"}
       ],
       test:false,
-      isactive:false,
-      iserror:true,
+      // isactive:false,
+      // iserror:true,
     }
   },
   methods:{
@@ -84,34 +91,57 @@ export default {
       let timer = window.setInterval(function(){
         if(vm.timer--<=0){
            vm.timer=10;
-           alert("timeup")
+           //times();
+           vm.times();
            window.clearInterval(timer);
         }
       },1000)
     },
-    sub(index){
+    sub(index,uid,isactive){
       console.log(index)
+      const key = document.querySelector(`.btn[uid="${uid}"]`)//獲得button的class和uid的屬性
+      console.log(key)
+      console.log(isactive)
+      this.$refs.reference.answer(isactive);
+      key.classList.add('playing')//添加class屬性playing配合css
+      key.addEventListener('transitionend',function(e){ //監聽transitionend(判斷css transition事件結束)，移除playing屬性，要呼叫vue的this要使用arrow function。
+         this.classList.remove('playing')
+       })
+      /*function removeadd(e){
+        this.classList.remove('playing')
+      }*/
+    },
+    button_(index){
+      return "button_"+index;
     }
   },
   mounted(){
     this.times();
+  },
+  components:{
+    answeranimation
   }
 };
 </script>
 <style scoped>
 .quizgame {
-  height: 100%;
+  /* padding-top: 5%; */
   width: 100%;
-  margin-top: 5%;
+  height: 100%;
+  position: absolute;
 }
-.progress {
+ .pro {
   margin-top: 5%;
-}
+}  
+.progress{
+  height: 40px;
+  font-size: 30px;
+} 
 .buttongroup{
   margin-top: 10%;
 }
 .timer{
-  margin-top: 16px;
+  margin-top: 25px;
 
 }
 /*.size{
@@ -150,13 +180,13 @@ export default {
   padding: 10px;
   padding-left: 20px;
   padding-right: 20px;
-   -moz-transition:0s 1s;
-   transition:0s 1s;
+   /*-moz-transition:0s 1s;
+   transition:0s 1s;*/ 
 } 
 p{
   font-size: 25px;
 }
-.correct:active {
+/* .correct:active {
   color: red;
   background: #3498db;
    -moz-transition:0s;
@@ -167,5 +197,58 @@ p{
   background: red;
    -moz-transition:0s;
    transition:0s;
+} */
+.playing{
+  border: 3px solid #FFC600;
+  transform: scale(1.5);
+  -webkit-transform: scale(1.5);
+   
+}
+@media screen and (min-width: 350px) and (max-width: 450px){
+  .buttongroup button{
+  display: block;
+   /*-moz-transition:0s 1s;
+   transition:0s 1s;*/ 
+   margin: 0 auto;
+} 
+.button_0,.button_1,.button_2,.button_3{
+   margin-bottom: 20px;
+}
+p{
+  font-size: 20px;
+}
+.timer{
+  margin-top: 20px;
+}
+.cardcol{
+  margin-top: -30px;
+}
+}
+@media screen and (min-width: 750px) and (max-width: 851px){
+  .buttongroup button{
+  display:block;
+   /*-moz-transition:0s 1s;
+   transition:0s 1s;*/ 
+   margin: 0px auto;
+   font-size: 40px;
+}
+.button_0,.button_1,.button_2,.button_3{
+   margin: 5%;
+   margin-bottom: 20px;
+}
+.progress{
+  height: 40px;
+  font-size: 30px;
+} 
+}
+@media screen and (min-width: 950px) and (max-width: 1200px){
+   .buttongroup button{
+   font-size: 30px;
+}
+  .quizgame {
+  height: 100%;
+  width: 100%;
+  margin-top: 25%;
+}
 }
 </style>
