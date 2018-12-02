2d4c77b44301d99258ff2814761253ef04a4970b
ZongWei <g0970216929@gmail.com>
2018-12-03 02:27:35 +0800

    router & graphql

--- src/main.js
+++ src/main.js
@@ -7,10 +7,27 @@ import HelloWorld from './components/HelloWorld'
 import loading from './components/loading'
 import quizgame from './components/quizgame'
 import secretpage from './components/secretpage'
+import endingpage from './components/Endingpage'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
 
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'http://yuansen.chengxun.tw/graphql',
+  })
+  // Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+  })
+  // Install the vue plugin
+Vue.use(VueApollo)
 Vue.use(BootstrapVue);
 Vue.use(VueRouter)
 
@@ -19,13 +36,18 @@ const router = new VueRouter({
   routes: [
     // 动态路径参数 以冒号开头
     { path: '/', component: main },
-    { path:'/HelloWorld',component:HelloWorld},
-    { path:'/quizgame',component:quizgame},
-    { path:'/loading',component:loading},
-    { path:'/secretpage',component:secretpage}
+    { path:'loading',name:'loading',component:loading},
+    { path:'/secretpage',component:secretpage},
+    { path:'/quizgame',name:'quizgame',component:quizgame},
+    { path:'endingpage',name:'endingpage',component:endingpage}
+
   ]
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
 })
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
