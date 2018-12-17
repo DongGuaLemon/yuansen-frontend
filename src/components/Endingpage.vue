0aee2238f50363583f27a7e3c50f40004fe0c05d
ZongWei <g0970216929@gmail.com>
2018-12-17 21:22:00 +0800

    endingpage finsish

--- src/components/Endingpage.vue
+++ src/components/Endingpage.vue
@@ -1,12 +1,13 @@
 <template>
 <div class="card">
-  <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="width:100%;height:550px">
+  <img src="https://yuan-sen.com.tw/uploads/images/%E5%AE%98%E6%96%B9%E9%A6%96%E9%A0%81(1).jpg" alt="Avatar" class="imgnice">
   <div class="container">
     <h4><b>恭喜您答對了{{this.$route.params.nice}}題</b></h4> 
-    <p>您獲得了..............................</p> 
+    <h3>您獲得了 商品折抵卷100元</h3>
+    <h4>序號:55ADAS25423</h4> 
   </div>
-  <b-button class="btn1">再玩一次</b-button>
-  <b-button>前往原生植物園</b-button>
+  <router-link :to="{path:'/'}"><b-button class="btn1" style="width:100%;">返回首頁</b-button></router-link>
+  <a href="https://www.yuan-sen.tw/"><b-button style="width:100%;">前往原生植物園</b-button></a>
 </div>  
 </template>
 <script>
@@ -18,7 +19,7 @@ export default {
   }
 }
 </script>
-<style>
+<style scoped>
 .card {
     margin: 0 auto;
     text-align: center;
@@ -36,5 +37,18 @@ export default {
 }
 .btn{
   margin-bottom:10px; 
+}
+.imgnice{
+  width:100%;
+  height:550px;
+}
+@media screen and (min-width: 350px) and (max-width: 450px){
+ .imgnice{
+   height: 350px;
+ } 
+ .card{
+   width: 300px;
+   height: 50%;
+ }
 }
 </style>
